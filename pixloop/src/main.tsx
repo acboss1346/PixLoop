74b5d47c5d87eabeac23c2e0a1e9afcbdf06a3da
Akshat Chauhan <akshatchauhan1346@gmail.com>
2025-05-06 22:32:33 +0530

    Your some commit

--- pixloop/src/main.tsx
+++ pixloop/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { BrowserRouter as Router } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { AuthProvider } from "./context/AuthContext.tsx";
+
+const client = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={client}>
+      {/* <AuthProvider> */}
+        <Router>
+          <App />
+        </Router>
+      {/* </AuthProvider> */}
+    </QueryClientProvider>
+  </React.StrictMode>
+);
